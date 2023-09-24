65942020d8f728dc2aba924d1bcd7181e9ac597e
Svezhii <137283496+Svezhii@users.noreply.github.com>
2023-09-24 16:46:06 +0300

    ---

--- Assets/Scenes/Scripts/Growth.cs
+++ Assets/Scenes/Scripts/Growth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CapsuleGrowth : MonoBehaviour
+public class Growth : MonoBehaviour
 {
     [SerializeField] private float _growthSpeed;
 
--- Assets/Scenes/Scripts/Rotation.cs
+++ Assets/Scenes/Scripts/Rotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeRotation : MonoBehaviour
+public class Rotation : MonoBehaviour
 {
     [SerializeField] private float _rotationSpeed;
 
